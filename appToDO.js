21706de42faeaeca4eac403b2b913f80cd8f6b26
arlindGH <ar1i66d@gmail.com>
2024-02-05 20:14:35 +0100

    Versioni im i lojes me zara!

--- appToDO.js
+++ appToDO.js
@@ -30,11 +30,16 @@ document.querySelector(".btn-roll").addEventListener("click",
 
 ()=>{
     if(!gameEnded){ // do not play a finished game
-    round = Math.floor(Math.random()*6)+1;
-    dice = document.querySelector(".dice");
-    dice.src="dice-"+round+".png";
-    dice.style.display="block";
-    if(round!==1){ // keep going
+    round1 = Math.floor(Math.random()*6)+1;
+    round2 = Math.floor(Math.random()*6)+1;
+    round = round1 + round2;
+    dice1 = document.querySelector(".dice1");
+    dice1.src="dice-"+round1+".png";
+    dice1.style.display="inline";
+    dice2 = document.querySelector(".dice2");
+    dice2.src="dice-"+round2+".png";
+    dice2.style.display="inline";
+    if(round1!==1 && round2!==1){ // keep going
         roundScore+=round;
         document.querySelector("#current-"+activePlayer).textContent=roundScore;
 
@@ -43,7 +48,8 @@ document.querySelector(".btn-roll").addEventListener("click",
         roundScore=0;
         document.querySelector("#current-"+activePlayer).textContent="0";
         activePlayer = (activePlayer+1)%2;
-        document.querySelector(".dice").style.display="none";
+        document.querySelector(".dice1").style.display="none";
+        document.querySelector(".dice2").style.display="none";
         document.querySelector(".player-0-panel").classList.toggle("active");
         document.querySelector(".player-1-panel").classList.toggle("active");
     }
@@ -59,7 +65,7 @@ if(!gameEnded){
     score[activePlayer] += roundScore;
     document.querySelector("#score-"+activePlayer).textContent=score[activePlayer];
 
-    if(score[activePlayer] >=100){
+    if(score[activePlayer] >=200){
 
         gameEnded=true;
         document.querySelector("#name-"+activePlayer).textContent="Winner!";
@@ -70,7 +76,8 @@ if(!gameEnded){
         roundScore=0;
         document.querySelector("#current-"+activePlayer).textContent="0";
         activePlayer = (activePlayer+1)%2;
-        document.querySelector(".dice").style.display="none";
+        document.querySelector(".dice1").style.display="none";
+        document.querySelector(".dice2").style.display="none";
         document.querySelector(".player-0-panel").classList.toggle("active");
         document.querySelector(".player-1-panel").classList.toggle("active");
     }
@@ -88,7 +95,8 @@ document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.querySelector("#current-0").textContent="0";
 document.querySelector("#current-1").textContent="0";
-document.querySelector(".dice").style.display="none";
+document.querySelector(".dice1").style.display="none";
+document.querySelector(".dice2").style.display="none";
 document.querySelector(".player-0-panel").classList.add("active");
 document.querySelector(".player-1-panel").classList.remove("active");
 document.querySelector(".player-0-panel").classList.remove("winner");
